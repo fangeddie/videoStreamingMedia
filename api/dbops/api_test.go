e366d9844b9555645fb20b82a0ef875084c9211c
Eddie <fangeddie@163.com>
2020-09-14 23:15:51 +0800

    feat(comment):comment api 完成一版

--- api/dbops/api_test.go
+++ api/dbops/api_test.go
@@ -1,7 +1,10 @@
 package dbops
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
+	"time"
 )
 
 // init(dblogin, truncate tables) -> run tests -> clear data(truncate tables)
@@ -96,4 +99,41 @@ func testReGetVideoInfo(t *testing.T) {
 	if err != nil || vi != nil {
 		t.Errorf("Error of RegetVideoInfo: %v", err)
 	}
+}
+
+func TestCommnets(t *testing.T) {
+	clearTables()
+	t.Run("AddUser", testAddUser)
+	t.Run("AddComments", testAddComments)
+	t.Run("ListComments", testListComments)
+}
+
+const (
+	vid  = "12345"
+	from = 1514764800
+)
+
+func testAddComments(t *testing.T) {
+	aid := 1
+	content := "I like this video"
+
+	err := AddNewComments(vid, aid, content)
+
+	if err != nil {
+		t.Errorf("Error of AddComments: %v", err)
+	}
+}
+
+func testListComments(t *testing.T) {
+	to, _ := strconv.Atoi(strconv.FormatInt(time.Now().UnixNano()/1000000000, 10))
+
+	res, err := ListComments(vid, from, to)
+	if err != nil {
+		t.Errorf("Error of ListComments: %v", err)
+	}
+
+	for i, ele := range res {
+		fmt.Printf("comment: %d, %v \n", i, ele)
+	}
+
 }
